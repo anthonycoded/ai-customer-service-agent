66199103d8018822940bb9587adf0cd2a5ed0fcc
Shawn <barbels33@gmail.com>
2026-08-18 06:53:27 -0400

    created auth headers for testing

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,8 +1,12 @@
 import os
 
+import pytest
+from fastapi.testclient import TestClient
+
 from app.database import Base, engine
 from app.models import Customer, Conversation, Message
 from app.models.user import User
+from app.main import app
 
 
 def pytest_configure():
@@ -16,3 +20,36 @@ def pytest_runtest_teardown(item, nextitem):
         connection.execute(Conversation.__table__.delete())
         connection.execute(Customer.__table__.delete())
         connection.execute(User.__table__.delete())
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+@pytest.fixture
+def auth_headers(client):
+    payload = {
+        "email": "test.auth@example.com",
+        "password": "TestPassword123!",
+    }
+
+    register_response = client.post(
+        "/auth/register",
+        json=payload,
+    )
+
+    assert register_response.status_code in [201, 400]
+
+    login_response = client.post(
+        "/auth/login",
+        json=payload,
+    )
+
+    assert login_response.status_code == 200
+
+    token = login_response.json()["access_token"]
+
+    return {
+        "Authorization": f"Bearer {token}"
+    }
